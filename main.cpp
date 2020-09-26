dd2c1db0c0d760d4723518e39a4f0d69b182479e
Faur Luciana <luciana_faur@yahoo.com>
2020-09-26 18:32:15 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ double l, aria;
+ cout << "Introduceti latura triunghiului echilateral: ";
+ cin >> l;
+
+ aria = (l * l * 1.73)/4;
+
+ cout << "Aria triunghiului este " << aria << endl;
+
+ return 0;
+}
+//calculate the area of the Equilateral Triangle :
